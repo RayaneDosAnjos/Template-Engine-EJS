ee85d3ecadd109ba3794e7ffe8593f4424da69ae
RayaneDosAnjos <raydosanjos98@gmail.com>
2023-02-28 18:28:15 -0300

    For no ejs

--- index.js
+++ index.js
@@ -3,16 +3,24 @@ const fs = require('fs')
 const app = express()
 const path = require('path')
 
-let user = {
+let users = [{
     name:"Rayane dos Anjos",
     phone:"(223)54321"
-}
+},
+    {
+        name: "Max Oliveira",
+        phone: "(223)54321"
+    },
+{
+        name: "Kelly Oliveira",
+        phone: "(223)54321"
+    }]
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res)=>{
-    res.render('user', user)
+    res.render('user', {users})
 })
 app.listen(3000, () =>{
     console.log("server Running on 3000")
